ced277693356e6ae1df2d85023984c8208195358
albert <albertgfdev@gmail.com>
2021-09-29 17:43:42 +0200

    update direction constructor

--- common/core/src/main/java/com/albertgf/core/Direction.kt
+++ common/core/src/main/java/com/albertgf/core/Direction.kt
@@ -1,9 +1,22 @@
 package com.albertgf.core
 
 import com.albertgf.core.extensions.fmod
+import java.lang.IllegalArgumentException
 
-class Direction {
-    private var current = 0
+class Direction (directionChar: String) {
+
+    private var current: Int
+
+    init {
+        current = when (directionChar) {
+            "N" -> 0
+            "E" -> 1
+            "S" -> 2
+            "W" -> 3
+            else -> throw IllegalArgumentException("Direction not valid")
+        }
+    }
+
     /*
     DIRECTIONS SORTED AS N E S W to work with clockwise and counterclockwise on turn
      */
