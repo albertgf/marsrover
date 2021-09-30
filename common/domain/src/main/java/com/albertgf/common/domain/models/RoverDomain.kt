c65e7c1abb00797f108a096175d7210842a5f64e
albert <albertgfdev@gmail.com>
2021-09-30 20:15:26 +0200

    add directions to rover domain

--- common/domain/src/main/java/com/albertgf/common/domain/models/RoverDomain.kt
+++ common/domain/src/main/java/com/albertgf/common/domain/models/RoverDomain.kt
@@ -1,10 +1,10 @@
 package com.albertgf.common.domain.models
 
-import androidx.compose.runtime.Composable
 
 data class RoverDomain (
     var terrain:Int = 0,
     var x:Int = 0,
     var y:Int = 0,
-    var instructions: String = ""
+    var instructions: String = "",
+    var direction: String = ""
 )
