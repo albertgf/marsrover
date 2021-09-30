ef9a15a5d9561f8a4faeba81d92d7b5eff7e272f
albert <albertgfdev@gmail.com>
2021-09-30 10:47:09 +0200

    remove move from terrain

--- common/core/src/main/java/com/albertgf/core/Terrain.kt
+++ common/core/src/main/java/com/albertgf/core/Terrain.kt
@@ -1,21 +1,10 @@
 package com.albertgf.core
 
-import arrow.core.None
-import arrow.core.Option
-import arrow.core.Some
 
 data class Terrain(
     val width: Int,
     val height: Int
 ) {
-    fun move(position: Position, direction: Direction): Option<Position> {
-        val x = position.x + direction.get().first
-        val y = position.y + direction.get().second
-
-        if ((x > width - 1 || y > height - 1) || x < 0 || y < 0) return None
-
-        return Some(position.update(direction))
-    }
      fun withinLimits(x: Int, y: Int): Boolean {
         return y <= height - 1 && y >= 0 && x < width && x >= 0
     }
