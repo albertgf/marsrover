f2b4394c03e877b14577e98a2c7a06fb2279c768
albert <albertgfdev@gmail.com>
2021-10-01 13:38:43 +0200

    style basic result screen over backdrop scafold

--- features/generator/src/main/java/com/albertgf/features/generator/GeneratorActivity.kt
+++ features/generator/src/main/java/com/albertgf/features/generator/GeneratorActivity.kt
@@ -3,23 +3,26 @@ package com.albertgf.features.generator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.albertgf.common.domain.models.Resource
+import com.albertgf.common.domain.models.ResourceState
 import com.albertgf.coreview.compose.BtnIconCircle
 import com.albertgf.coreview.compose.DirectionsInput
 import com.albertgf.coreview.compose.InputField
@@ -36,16 +39,22 @@ class GeneratorActivity : ComponentActivity() {
 
         setContent {
             MarsroverTheme {
-                val scaffoldState = rememberBackdropScaffoldState(initialValue = BackdropValue.Concealed)
+                val scaffoldState =
+                    rememberBackdropScaffoldState(initialValue = BackdropValue.Concealed)
                 val scope = rememberCoroutineScope()
                 // A surface container using the 'background' color from the theme
                 BackdropScaffold(
                     scaffoldState = scaffoldState,
-                    appBar = { AppBar()  },
-                    backLayerContent = { ScreenConnection { scope.launch { scaffoldState.conceal() }} },
+                    appBar = { AppBar() },
+                    backLayerContent = { ScreenConnection(viewModel.result.collectAsState()) { scope.launch { scaffoldState.conceal() } } },
+                    backLayerContentColor = Color.White,
+                    backLayerBackgroundColor = Color.DarkGray,
                     frontLayerContent = {
                         ScreenGenerator { scope.launch { scaffoldState.reveal() } }
                     },
+                    frontLayerContentColor = Color.LightGray,
+                    frontLayerScrimColor = Color.LightGray,
+                    frontLayerBackgroundColor = Color.LightGray,
                     gesturesEnabled = false
                 ) {
 
@@ -55,17 +64,98 @@ class GeneratorActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ScreenConnection(onReveal: () -> Unit) {
-        Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center) {
-            Button(onClick = {
-                onReveal()
-            }) {
-                Text("SEND CONCEAL")
+    fun ScreenConnection(data: State<Resource<String>>, onReveal: () -> Unit) {
+        Surface(
+            color = Color.Transparent
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sending Data")
+                Icon(
+                    imageVector = imageSending(data.value.state),
+                    contentDescription = "",
+                    tint = imageSendingColor(data.value.state),
+                    modifier =
+                    Modifier
+                        .padding(16.dp)
+                        .height(64.dp)
+                        .width(64.dp)
+                )
+                Text("Explore")
+                Icon(
+                    imageVector = imageExplore(data.value.state),
+                    contentDescription = "",
+                    tint = imageExploreColor(data.value.state),
+                    modifier =
+                    Modifier
+                        .padding(16.dp)
+                        .height(64.dp)
+                        .width(64.dp)
+                )
+                Text("Ending Position")
 
+                Box(
+                    modifier = Modifier.clip(shape = CircleShape).background(Color.Black).padding(8.dp).defaultMinSize(100.dp, 32.dp)
+                ) {
+                    Text(
+                        text = data.value.data ?: "",
+                        textAlign = TextAlign.Center,
+                        fontSize = 32.sp,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
+
+                Button(onClick = {
+                    onReveal()
+                }) {
+                    Text(
+                        "SEND MORE DATA",
+                        color = MaterialTheme.colors.onPrimary
+                    )
+
+                }
             }
         }
+    }
 
+    private fun imageSending(state: ResourceState): ImageVector {
+        return when (state) {
+            ResourceState.IDLE -> Icons.Filled.WifiOff
+            ResourceState.SETUP -> Icons.Filled.Wifi
+            ResourceState.LOADING -> Icons.Filled.Wifi
+            ResourceState.SUCCESS -> Icons.Filled.Wifi
+        }
+    }
+
+    private fun imageSendingColor(state: ResourceState): Color {
+        return when (state) {
+            ResourceState.IDLE -> Color.White
+            ResourceState.SETUP -> Color.White
+            ResourceState.LOADING -> Color.Green
+            ResourceState.SUCCESS -> Color.Green
+        }
+    }
+
+    private fun imageExplore(state: ResourceState): ImageVector {
+        return when (state) {
+            ResourceState.IDLE -> Icons.Filled.Pause
+            ResourceState.SETUP -> Icons.Filled.Pause
+            ResourceState.LOADING -> Icons.Filled.Explore
+            ResourceState.SUCCESS -> Icons.Filled.Explore
+        }
+    }
+
+    private fun imageExploreColor(state: ResourceState): Color {
+        return when (state) {
+            ResourceState.IDLE -> Color.Red
+            ResourceState.SETUP -> Color.Red
+            ResourceState.LOADING -> Color.White
+            ResourceState.SUCCESS -> Color.Green
+        }
     }
 
     @Composable
@@ -138,8 +228,8 @@ class GeneratorActivity : ComponentActivity() {
                 onDirectionChange = { viewModel.point(it) })
 
             Button(onClick = {
-                viewModel.sendData()
                 onConceal()
+                viewModel.sendData()
             }) {
                 Text("SEND DATA")
 
