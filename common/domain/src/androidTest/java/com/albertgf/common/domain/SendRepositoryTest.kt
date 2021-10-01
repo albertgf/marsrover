0e3771b57829be9e7aca447672323eb43b3075a4
albert <albertgfdev@gmail.com>
2021-10-01 19:45:07 +0200

    update repository test with flow response

--- common/domain/src/androidTest/java/com/albertgf/common/domain/SendRepositoryTest.kt
+++ common/domain/src/androidTest/java/com/albertgf/common/domain/SendRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.albertgf.common.domain
 
+import com.albertgf.common.domain.models.ResourceState
 import com.albertgf.common.domain.repository.SendRepository
 import com.albertgf.core.Rover
 import io.mockk.confirmVerified
@@ -8,14 +9,15 @@ import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
-
+import org.junit.Assert
 import org.junit.Test
-
 import org.junit.Before
 
 /**
@@ -43,17 +45,41 @@ class SendRepositoryTest {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun verify_rover_setup_and_call_explore() = runBlockingTest {
+    fun verify_rover_setup_and_call_explore() = testDispatcher.runBlockingTest {
         val repository = SendRepository(mockRover, testDispatcher)
 
         every { mockRover.setup(any()) } returns Unit
         every { mockRover.explore() } returns "1 3 N"
 
-        repository.send(DomainTestUtil.fakeRoverDomain())
+        val flow = repository.send(DomainTestUtil.fakeRoverDomain())
+
+        flow.collect{
+
+        }
 
         verify { mockRover.setup(DomainTestUtil.fakeRoverDataParserString()) }
         verify { mockRover.explore() }
 
         confirmVerified(mockRover)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun verify_emit_all_states() = testDispatcher.runBlockingTest {
+        val repository = SendRepository(mockRover, testDispatcher)
+
+        every { mockRover.setup(any()) } returns Unit
+        every { mockRover.explore() } returns "1 3 N"
+
+        val flow = repository.send(DomainTestUtil.fakeRoverDomain())
+
+        flow.collectIndexed { index, value ->
+            run {
+                if (index == 0) Assert.assertTrue(value.state is ResourceState.IDLE)
+                if (index == 1) Assert.assertTrue(value.state is ResourceState.SETUP)
+                if (index == 2) Assert.assertTrue(value.state is ResourceState.LOADING)
+                if (index == 3) Assert.assertTrue(value.state is ResourceState.SUCCESS)
+            }
+        }
     }
 }
